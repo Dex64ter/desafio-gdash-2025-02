24da913182dbbad243a6afe6b8758dcd74e6dd1a
Davi José Cunha Santos <davijosantos.dj@gmail.com>
2025-12-04 09:44:37 -0300

    containerização da aplicação fullstack

--- collector/collector.py
+++ collector/collector.py
@@ -1,46 +1,69 @@
 import os
 import json
-from time import time
+import time
 import pika
 import requests
 
-RABBITMQ_URL = "amqp://rabbitmq:5672"
-QUEUE_NAME = "data_queue"
+RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+QUEUE_NAME = "weather-data"
+
+
 def connect_rabbit():
-  params = pika.URLParameters(RABBITMQ_URL)
-  connection = pika.BlockingConnection(params)
-  channel = connection.channel()
-  channel.queue_declare(queue="weather-data")
-  return connection, channel
+    try:
+        print(f"🔌 Tentando conectar ao RabbitMQ: {RABBITMQ_URL}")
+        
+        params = pika.URLParameters(RABBITMQ_URL)
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
+        print("✔ Conectado ao RabbitMQ")
+        return connection, channel
+
+    except Exception as e:
+        print(f"❌ Falha ao conectar ao RabbitMQ: {e}")
+        print("⏳ Tentando novamente em 5s...")
+        time.sleep(5)
+
 
 def fetch_weather():
-    url = ("https://api.open-meteo.com/v1/forecast?latitude=-5&longitude=-42.41&current_weather=true&timezone=America%2FSao_Paulo")
+    url = (
+        "https://api.open-meteo.com/v1/forecast?"
+        "latitude=-5&longitude=-42.41&current_weather=true&timezone=America%2FSao_Paulo"
+    )
     response = requests.get(url)
     return response.json()["current_weather"]
 
+
 def main():
-    connection, channel = connect_rabbit()
     print("Collector iniciado...")
 
+    connection, channel = connect_rabbit()
+
     while True:
-        weather = fetch_weather()
+        try:
+            weather = fetch_weather()
 
-        payload = {
-            "temperature": weather["temperature"],
-            "windspeed": weather["windspeed"],
-            "winddirection": weather["winddirection"],
-            "time": weather["time"],
-        }
+            payload = json.dumps(weather)
 
-        channel.basic_publish(
-            exchange="",
-            routing_key="weather-data",
-            body=json.dumps(payload),
-        )
+            channel.basic_publish(
+                exchange="",
+                routing_key=QUEUE_NAME,
+                body=payload,
+            )
 
-        print("Enviado para a fila:", payload)
+            print("✔ Mensagem enviada:", weather)
 
-        time.sleep(5000)
+            time.sleep(10)
+
+        except (pika.exceptions.AMQPConnectionError, pika.exceptions.StreamLostError):
+            print("⚠ Conexão perdida! Tentando reconectar...")
+            connection, channel = connect_rabbit()
+
+        except Exception as e:
+            print(f"❌ Erro inesperado: {e}")
+            time.sleep(5)
+
 
 if __name__ == "__main__":
     main()
